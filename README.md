5ee4e981eadfce850c5b046c48f1b28c0ca340fe
jerryorta.dev@gmail.com <jerryorta.dev@gmail.com>
2022-06-20 06:05:27 -0500

    wip

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Generate Angular Nx Workspace
 
+A bash script to create an angular-nest workspace with predefined libraries.
+
 ## To run:
 
 ```
-    
+    bash <(curl -sL https://raw.githubusercontent.com/UIUXEngineering/nx-ng-workspace/main/generate.sh)
 ```
+
+## Libraries added
+
+- Angular Material
+- NgRX
+- Firebase
--- generate.sh
+++ generate.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-# TO RUN
-# bash <(curl -sL https://gist.github.com/jerryorta-dev/ee1c8009ac923e37e0c18a84d9566616/raw/nx-ng-workspace.sh)
+# https://www.baeldung.com/linux/use-command-line-arguments-in-bash-script
+while getopts workspace-name: flag
+do
+    case "${flag}" in
+        workspace-name) workspaceName=${OPTARG};;
+    esac
+done
 
 # Create Nx Workspace
-npx create-nx-workspace@latest
+npx create-nx-workspace@latest --preset=angular-nest
+
+
 
 # Add Material
 npm install @angular/material && npx nx g @angular/material:ng-add
